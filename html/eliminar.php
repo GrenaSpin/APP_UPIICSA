fc7bc7b9e533e2c742e3098e0558a2c8f8f95335
Yael López Gómez <yaellopezgomez2013@gmail.com>
2025-12-29 22:33:28 -0600

    Archivos borrados

--- html/eliminar.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id = $_GET["id"];
-
-if (file_exists("presentaciones/$id")) {
-    unlink("presentaciones/$id");
-}
-
-header("Location: index.php");
-exit;
--- includes/sidebar.php
+++
@@ -1,7 +0,0 @@
-<aside id="sidebar" class="sidebar">
-    <ul class="sidebar-menu">
-        <li><a href="index.php">Material de clase</a></li>
-        <li><a href="materias.php">Materias</a></li>
-        <li><a href="settings.php">Diseño</a></li>
-    </ul>
-</aside>
